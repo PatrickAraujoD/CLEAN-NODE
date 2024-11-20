63ae18c48c52d4e9f453b0ac53d70cf4ac1a275b
PatrickAraujoD <lordpatrick200@gmail.com>
2024-11-20 18:57:12 -0300

    feat: ensure SingUp route returns an account on sucess

--- src/main/config/app.ts
+++ src/main/config/app.ts
@@ -1,6 +1,8 @@
 import express from 'express'
 import setupMiddlewares from './middlewares'
+import setupRouters from './routes'
 
 const app = express()
 setupMiddlewares(app)
+setupRouters(app)
 export default app

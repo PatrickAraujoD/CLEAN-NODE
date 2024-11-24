b31de2ce98c3525c4965e6377a4547fa90ea0816
PatrickAraujoD <lordpatrick200@gmail.com>
2024-11-24 10:28:06 -0300

    refactor: remove dependency and unused instance

--- src/main/factories/signup.ts
+++ src/main/factories/signup.ts
@@ -5,7 +5,6 @@ import { AccountMongoRepository } from '../../infra/db/mongodb/account-repositor
 import { Controller } from '../../presentation/protocols'
 import { LogControllerDecorator } from '../decorators/log'
 import { LogMongoRepository } from '../../infra/db/mongodb/log-repository/log'
-import { ValidationComposite } from '../../presentation/helpers/validators/validation-compose'
 import { makeSignUpValidation } from './singup-validation'
 
 export const makeSignUpController = (): Controller => {
@@ -14,9 +13,6 @@ export const makeSignUpController = (): Controller => {
   const accountMongoRepository = new AccountMongoRepository()
   const logErrorRepository = new LogMongoRepository()
   const dbAddAccount = new DbAddAccount(bcryptAdapter, accountMongoRepository)
-  const validationComposite = new ValidationComposite([
-
-  ])
   const singUpController = new SignUpController(dbAddAccount, makeSignUpValidation())
   return new LogControllerDecorator(singUpController, logErrorRepository)
 }

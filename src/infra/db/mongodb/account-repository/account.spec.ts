f9f2942c72a4582d2ac00f85953e66edccaa9e1b
PatrickAraujoD <lordpatrick200@gmail.com>
2024-12-31 19:38:18 -0300

    test: ensure AccountMongoRepository returns null if loadByEmail fails

--- src/infra/db/mongodb/account-repository/account.spec.ts
+++ src/infra/db/mongodb/account-repository/account.spec.ts
@@ -45,11 +45,16 @@ describe("Account Mongo Reposiotry", () => {
       password: 'any_password'
     })
     const account = await sut.loadByEmail('any_email@mail.com')
-
     expect(account).toBeTruthy()
     expect(account.id).toBeTruthy()
     expect(account.name).toBe('any_name')
     expect(account.email).toBe('any_email@mail.com')
     expect(account.password).toBe('any_password')
+  })
+
+  test("Should return null if loadByEmail fails", async () => {
+    const sut = makeSut();
+    const account = await sut.loadByEmail('any_email@mail.com')
+    expect(account).toBeFalsy()
   })
 })

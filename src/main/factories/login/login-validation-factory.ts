0e4bc6c7950611f77d6e3438856f809dec066773
PatrickAraujoD <lordpatrick200@gmail.com>
2025-01-02 09:52:45 -0300

    feat: add login route

--- src/main/factories/login/login-validation-factory.ts
+++ src/main/factories/login/login-validation-factory.ts
@@ -6,6 +6,6 @@ export const makeLoginValidation = (): ValidationComposite => {
   for (const field of ["email", "password"]) {
     validations.push(new RequiredFieldValidation(field))
   }
-  validations.push(new EmailValidation('email', new EmailValidatorAdapter))
+  validations.push(new EmailValidation('email', new EmailValidatorAdapter()))
   return new ValidationComposite(validations)
 }

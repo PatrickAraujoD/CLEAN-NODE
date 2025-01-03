a23b12b42653414f8cafa0cef750b7b1db9e0d77
PatrickAraujoD <lordpatrick200@gmail.com>
2025-01-03 17:56:49 -0300

    test: ensure AuthMiddleware calls LoadAccountByToken with correct token

--- src/presentation/middlewares/auth-middleware.spec.ts
+++ src/presentation/middlewares/auth-middleware.spec.ts
@@ -1,11 +1,45 @@
+import { AccountModel } from "../../domain/models/account"
+import { LoadAccountByToken } from "../../domain/usecases/load-account-by-token"
 import { AccessDeniedError } from "../errors"
 import { forbidden } from "../helpers/http/http-helper"
 import { AuthMiddleware } from "./auth-middleware"
 
+const makeFakeAccount = (): AccountModel => ({
+  id: "any_id",
+  name: "any_name",
+  email: "any_email@mail.com",
+  password: "any_password"
+})
+
 describe('Auth Middleware', () => {
   test('Should return 403 if no x-access-token exists in headers', async () => {
-    const sut = new AuthMiddleware()
+    class LoadAccountByTokenStub implements LoadAccountByToken {
+      load(acessToken: string, role?: string): Promise<AccountModel> {
+        const account = makeFakeAccount()
+        return new Promise(resolve => resolve(account))
+      }
+    }
+    const loadAccountByTokenStub = new LoadAccountByTokenStub()
+    const sut = new AuthMiddleware(loadAccountByTokenStub)
     const httpResponse = await sut.handle({})
     expect(httpResponse).toEqual(forbidden(new AccessDeniedError()))
+  })
+
+  test('Should call LoadAccountByToken with correct accessToken', async () => {
+    class LoadAccountByTokenStub implements LoadAccountByToken {
+      load(acessToken: string, role?: string): Promise<AccountModel> {
+        const account = makeFakeAccount()
+        return new Promise(resolve => resolve(account))
+      }
+    }
+    const loadAccountByTokenStub = new LoadAccountByTokenStub()
+    const sut = new AuthMiddleware(loadAccountByTokenStub)
+    const loadSpy = jest.spyOn(loadAccountByTokenStub, 'load')
+    const httpResponse = await sut.handle({
+      headers: {
+        'x-access-token': 'any_token' 
+      }
+    })
+    expect(loadSpy).toHaveBeenCalledWith('any_token')
   })
 })

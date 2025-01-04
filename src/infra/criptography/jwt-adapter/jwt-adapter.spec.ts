8a13a8585dc5e001a54a27addecb94d62e2fc71d
PatrickAraujoD <lordpatrick200@gmail.com>
2025-01-04 10:42:58 -0300

    test: ensure JwtAdapter call verify with correct values

--- src/infra/criptography/jwt-adapter/jwt-adapter.spec.ts
+++ src/infra/criptography/jwt-adapter/jwt-adapter.spec.ts
@@ -4,6 +4,10 @@ import { JwtAdapter } from './jwt-adapter'
 jest.mock('jsonwebtoken', () => ({
   async sign (): Promise<string> {
     return new Promise(resolve => resolve('any_token'))
+  },
+
+  async verify (): Promise<string> {
+    return new Promise(resolve => resolve('any_value'))
   }
 }))
 
@@ -18,21 +22,30 @@ describe('Jwt Adapter', () => {
       const signSpy = jest.spyOn(jwt, 'sign')
       await sut.encrypt('any_id')
       expect(signSpy).toHaveBeenCalledWith({ id: 'any_id' }, 'secret')
+    })
+  
+    test('Should return a token on sign success', async () => {
+      const sut = makeSut()
+      const acessToken = await sut.encrypt('any_id')
+      expect(acessToken).toBe('any_token')
+    })
+  
+    test('Should throw if sign throws', async () => {
+      const sut = makeSut()
+      jest.spyOn(jwt, 'sign').mockImplementationOnce(() => {
+        throw new Error()
       })
-    
-      test('Should return a token on sign success', async () => {
-        const sut = makeSut()
-        const acessToken = await sut.encrypt('any_id')
-        expect(acessToken).toBe('any_token')
-      })
-    
-      test('Should throw if sign throws', async () => {
-        const sut = makeSut()
-        jest.spyOn(jwt, 'sign').mockImplementationOnce(() => {
-          throw new Error()
-        })
-        const promise = sut.encrypt('any_id')
-        await expect(promise).rejects.toThrow()
-      })
+      const promise = sut.encrypt('any_id')
+      await expect(promise).rejects.toThrow()
+    })
+  })
+
+  describe('verify()', () => {
+    test('Should call verify with correct values', async () => {
+      const sut = makeSut()
+      const verifySpy = jest.spyOn(jwt, 'verify')
+      await sut.decrypt('any_token')
+      expect(verifySpy).toHaveBeenCalledWith('any_token', 'secret')
+    })
   })
 })

bd29e0970c6350d09f0428d02b2dcc33a2dccddc
PatrickAraujoD <lordpatrick200@gmail.com>
2025-01-04 17:24:04 -0300

    test: ensure add survey route returns 204 if valid access token is provided

--- src/main/routes/survey-routes.test.ts
+++ src/main/routes/survey-routes.test.ts
@@ -1,9 +1,12 @@
 import request from 'supertest'
 import app from '../config/app'
+import env from '../config/env'
+import { sign } from 'jsonwebtoken';
 import { MongoHelper } from '../../infra/db/mongodb/helpers/mongo-helper'
 import { Collection } from 'mongodb'
 
 let serveyCollection: Collection
+let accountCollection: Collection
 
 describe('Survey Routes', () => {
   beforeAll(async () => {
@@ -16,7 +19,9 @@ describe('Survey Routes', () => {
 
   beforeEach(async () => {
     serveyCollection = await MongoHelper.getCollection('surveys')
+    accountCollection = await MongoHelper.getCollection('accounts')
     await serveyCollection.deleteMany({})
+    await accountCollection.deleteMany({})
   })
 
   describe("POST /surveys", () => {
@@ -33,6 +38,40 @@ describe('Survey Routes', () => {
               }]
             })
             .expect(403)
+    })
+
+    test('Should return 204 on add survey with valid accessToken', async () => {
+      const res = await accountCollection.insertOne({
+        name: "patrick",
+        email: "mail@mail.com",
+        password: '123',
+        role: 'admin'
+      })
+      const id = res.insertedId
+      const accessToken = sign({ id }, env.jwtSecret)
+      await accountCollection.updateOne(
+        {
+          _id: id
+        }, {
+          $set: {
+            accessToken
+          }
+        }
+      )
+
+      await request(app)
+            .post('/api/surveys')
+            .set('x-access-token', accessToken)
+            .send({
+              question: 'Question',
+              answers: [{
+                answer: 'Answer 1',
+                image: 'http://image-name.com'
+              }, {
+                answer: 'Answer 2'
+              }]
+            })
+            .expect(204)
     })
   })
 })

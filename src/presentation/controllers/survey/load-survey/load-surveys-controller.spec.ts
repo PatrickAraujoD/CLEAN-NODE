e838ff43f488c60dea137293ec6a876ef7d40912
PatrickAraujoD <lordpatrick200@gmail.com>
2025-01-11 18:28:15 -0300

    test: ensure LoadSurveysController retruns 500 if LoadSurveysUseCase throws

--- src/presentation/controllers/survey/load-survey/load-surveys-controller.spec.ts
+++ src/presentation/controllers/survey/load-survey/load-surveys-controller.spec.ts
@@ -1,7 +1,7 @@
 import { LoadSurveys, SurveyModel } from "./load-surveys-controller-protocols"
 import { LoadSurveysController } from "./load-surveys-controller"
 import MockDate from "mockdate"
-import { ok } from "../../../helpers/http/http-helper"
+import { ok, serverError } from "../../../helpers/http/http-helper"
 
 const makeLoadSurveys = (): LoadSurveys => {
   class LoadSurveysStub implements LoadSurveys {
@@ -67,5 +67,12 @@ describe('LoadSurveys Controller', () => {
     const { sut } = makeSut()
     const httpResponse = await sut.handle({})
     expect(httpResponse).toEqual(ok(makeFakeSurveys()))
+  })
+
+  test('Should return 500 if AddSurvey throws', async () => {
+      const { sut, loadSurveysStub } = makeSut()
+      jest.spyOn(loadSurveysStub, 'load').mockReturnValueOnce(new Promise((resolve, reject) => reject(new Error())))
+      const promise = await sut.handle({})
+      expect(promise).toEqual(serverError(new Error()))
   })
 })

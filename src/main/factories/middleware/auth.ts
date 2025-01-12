3265b79ba490e0a20ab361eb3068676e434668f3
PatrickAraujoD <lordpatrick200@gmail.com>
2025-01-11 22:07:07 -0300

    refactor: move auth middlewares to separate files

---
+++ src/main/factories/middleware/auth.ts
@@ -0,0 +1,5 @@
+import { adaptMiddleware } from "../../adapters/express-middleware-adapter";
+import { makeAuthMiddleware } from "./auth-middleware.-factory";
+
+export   
+  const auth = adaptMiddleware(makeAuthMiddleware())
